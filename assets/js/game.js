7a75f9ad6fbf7ea54c37184e0c7583bd52bf8be3
gareth-39 <Gareth.A.Henvey@gmail.com>
2022-11-18 20:09:34 +0000

    Fixing a bug with my save button

--- assets/js/game.js
+++ assets/js/game.js
@@ -185,6 +185,16 @@ choices.forEach(choice => {
     score += num
     scoreText.innerText = score
 }
+const mediaQuery = window.matchMedia('(min-width: 768px)')// Create a condition that targets viewports at least 768px wide
+
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+    console.log('Media Query Matched!')
+  }
+}
 
 startGame()
 
